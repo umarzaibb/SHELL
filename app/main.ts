253d1170bec577c4520448f9de90223bb4a48d6c
Umar Zaib <umarzzaib2@gmail.com>
2025-12-29 20:50:19 +0500

    Print a prompt.

--- app/main.ts
+++ app/main.ts
@@ -6,6 +6,6 @@ const rl = createInterface({
 });
 
 // TODO: Uncomment the code below to pass the first stage
-// rl.question("$ ", (answer) => {
-//   rl.close();
-// });
+rl.question("$ ", (answer) => {
+  rl.close();
+});
