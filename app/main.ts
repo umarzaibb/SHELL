c007799e47fb3560e5f25f23798db36740aca7dd
Umar Zaib <umarzzaib2@gmail.com>
2025-12-29 21:56:22 +0500

    Implemented type.

--- app/main.ts
+++ app/main.ts
@@ -7,6 +7,7 @@ const rl = createInterface({
 
 // TODO: Uncomment the code below to pass the first stage
 
+let commands:String[]= ['echo', 'exit'];
 
  async function askPrompt() {
 
@@ -19,7 +20,15 @@ const rl = createInterface({
 
         if(answer.indexOf('echo')===0) {
           console.log(answer.slice(5));
-        }else{
+        }
+
+        else if(answer.indexOf('type')===0) {
+           let curr_command= answer.split(" ")[1];
+           if(commands.includes(curr_command)) {
+            console.log(`${curr_command} is a shell builtin`);
+           }
+        }
+        else{
           console.log(`${answer}: command not found`);
         }
         askPrompt();
