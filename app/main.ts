3a55c60e71973c44f09354a56ac3e8051e432c91
Umar Zaib <umarzzaib2@gmail.com>
2025-12-30 17:01:50 +0500

    Locate executable file.

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,6 @@
 import { createInterface } from "readline";
+import { access, constants } from "fs";
+import path from 'node:path';
 
 const rl = createInterface({
   input: process.stdin,
@@ -8,11 +10,12 @@ const rl = createInterface({
 // TODO: Uncomment the code below to pass the first stage
 
 let commands:String[]= ['echo', 'exit', 'type'];
-console.log(process.env.PATH);
+let curr_path= process.env.PATH;
+let curr_path_directories= curr_path?.split(`${path.delimiter}`);
 
  async function askPrompt() {
 
-    let callback= (answer)=>{
+    let callback= async(answer)=>{
 
         if(answer==='exit') {
           rl.close();
@@ -24,12 +27,29 @@ console.log(process.env.PATH);
         }
 
         else if(answer.indexOf('type')===0) {
+          console.log(process.env.PATH);
            let curr_command= answer.split(" ")[1];
            if(commands.includes(curr_command)) {
             console.log(`${curr_command} is a shell builtin`);
            }
+           
+           let isExecutableExists=false;
+           for(let i of curr_path_directories) {
+            if(isExecutableExists) {
+              break;
+            }
+            try {
+
+               await access(`${i}/${curr_command}`, constants.R_OK | constants.W_OK);
+               isExecutableExists=true;
+               console.log(`${curr_command} is ${curr_path}`);
+            } catch {
+               
+            } 
+           }
+           
         
-            else{
+           if(!commands.includes(curr_command) && !isExecutableExists) {
           console.log(`${curr_command}: not found`);
         }
         }
