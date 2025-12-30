5174066a4646ebe83651b437517882ce4cdb433c
Umar Zaib <umarzzaib2@gmail.com>
2025-12-30 12:10:38 +0500

    Locate executable file.

--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,4 @@
 import { createInterface } from "readline";
-import path from 'node:path';
-import { dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
 
 const rl = createInterface({
   input: process.stdin,
@@ -11,7 +8,7 @@ const rl = createInterface({
 // TODO: Uncomment the code below to pass the first stage
 
 let commands:String[]= ['echo', 'exit', 'type'];
-let curr_path= dirname(fileURLToPath(import.meta.url));
+console.log(process.env.PATH);
 
  async function askPrompt() {
 
@@ -27,14 +24,11 @@ let curr_path= dirname(fileURLToPath(import.meta.url));
         }
 
         else if(answer.indexOf('type')===0) {
-          console.log(curr_path);
            let curr_command= answer.split(" ")[1];
            if(commands.includes(curr_command)) {
             console.log(`${curr_command} is a shell builtin`);
            }
-           else if(curr_path?.includes(curr_command)) {
-            console.log(`${curr_command} is ${curr_command}`);
-           }
+        
             else{
           console.log(`${curr_command}: not found`);
         }
